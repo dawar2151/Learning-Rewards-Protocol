a374fac196beb7493199bfc67adcd09ba527ee74
zouhair et-taousy <zouhirtaousy@gmail.com>
2025-01-18 17:43:46 +0100

    feat: add clean rewards

--- components/rewards/RewardsCalculator.tsx
+++ components/rewards/RewardsCalculator.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Button } from '@headlessui/react';
 import React, { useEffect, useState } from 'react';
 import { useAccount } from 'wagmi';
 
@@ -9,7 +10,8 @@ const RewardsCalculator: React.FC = () => {
     const [rewardDistributed, setRewardDistributed] = useState(false);
     const [loading, setLoading] = useState(false);
     const [txHash, settxHash] = useState<string | null>(null);
-    const [signedMessage, setSignedMessage] = useState<string>('');
+    const [readingTime] = useState<number>(120);
+    const [signedMessage] = useState<string>('');
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -19,11 +21,6 @@ const RewardsCalculator: React.FC = () => {
         return () => clearInterval(interval);
     }, []);
 
-    useEffect(() => {
-        if (timeSpent >= 20 && !rewardDistributed) {
-            distributeReward();
-        }
-    }, [timeSpent, rewardDistributed]);
 
     const distributeReward = async () => {
         setLoading(true);
@@ -49,21 +46,31 @@ const RewardsCalculator: React.FC = () => {
     };
 
     return (
-        <div>
-            <h1>Rewards Calculator</h1>
-            {!rewardDistributed && <p>Time spent on page: {timeSpent} seconds</p>}
-            {loading && <p>Loading...</p>}
-            {rewardDistributed && (
-                <div>
-                    <p>Reward has been distributed!</p>
-                    {txHash && (
-                        <p>
-                            Check transaction on Etherscan: <a href={`https://sepolia.etherscan.io/tx/${txHash}`} target="_blank" rel="noopener noreferrer">{txHash}</a>
-                        </p>
-                    )}
-                </div>
+        <div className="flex flex-col items-center justify-center">
+      <div className="bg-white shadow-md rounded-lg p-6 w-full max-h-90">
+        <h1 className="text-2xl font-semibold text-gray-800 mb-4">Rewards Calculator</h1>
+        {timeSpent <  readingTime && <p className="text-xl text-gray-600"> Reading Time Spent: {Math.floor(timeSpent / 60)} minutes {timeSpent % 60} seconds</p>}
+       
+        {loading && <p className="text-xl text-gray-600">Loading...</p>}
+        {rewardDistributed && (
+          <div>
+            <p className="text-xl text-green-600">Reward has been distributed!</p>
+            {txHash && (
+              <p className="text-xl text-blue-600">
+                Check transaction on Etherscan: <a href={`https://sepolia.etherscan.io/tx/${txHash}`} target="_blank" rel="noopener noreferrer">{txHash}</a>
+              </p>
             )}
-        </div>
+          </div>
+        )}
+        {timeSpent >= readingTime && !loading && (
+          <Button
+            onClick={distributeReward}
+            className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"          >
+            Cleam Rewards
+          </Button>
+        )}
+      </div>
+    </div>
     );
 };
 

fbe2f41e65e50460c5475810db399fa3d6c1fd65
Zouhair ET <zouhair.et.taousy@gmail.com>
2025-07-28 01:14:45 +0200

    fix: update favicon links to use a single favicon.ico file

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,19 +72,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link
         rel="apple-touch-icon"
         sizes="76x76"
-        href={`${basePath}/static/favicons/apple-touch-icon.png`}
+        href={`${basePath}/static/favicons/favicon.ico`}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href={`${basePath}/static/favicons/favicon-32x32.png`}
+        href={`${basePath}/static/favicons/favicon.ico`}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href={`${basePath}/static/favicons/favicon-16x16.png`}
+        href={`${basePath}/static/favicons/favicon.ico`}
       />
       <link rel="manifest" href={`${basePath}/static/favicons/site.webmanifest`} />
       <link
